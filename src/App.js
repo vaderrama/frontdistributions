a2472898c98fbaffb065be78b721efe25f52bd32
vaderrama <juan.alvarezz.11@gmail.com>
2021-09-21 21:27:11 +0200

    commit front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./components/home";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Area, AreaChart,  } from 'recharts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+ render () {
+   return (
+     <div>
+        <Home/>
+      </div>
+      
+    );
+  }
 }
-
-export default App;
+export default App
